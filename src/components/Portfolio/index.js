16d83e003cda1460748cae54f965e310cbf433f5
George Colon <geroge_colon@oneilluminate.com>
2023-02-02 08:08:27 -0800

    save changes

--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -2,31 +2,31 @@ import React, { useEffect, useState } from "react";
 import Loader from "react-loaders";
 import AnimatedLetters from "../AnimatedLetters";
 import "./index.scss";
-import { getDocs, collection } from 'firebase/firestore';
-import { db } from '../../firebase';
+// import { getDocs, collection } from 'firebase/firestore';
+// import { db } from '../../firebase';
 
 const Portfolio = () => { 
-    const [letterClass, setLetterClass] = useState('text-animate');
-    const [portfolio, setPortfolio] = useState([]);
+    // const [letterClass, setLetterClass] = useState('text-animate');
+    // const [portfolio, setPortfolio] = useState([]);
 
-    useEffect(() => {
-        const timer = setTimeout(() => {
-            setLetterClass('text-animate-hover');
-        }, 3000);
+    // useEffect(() => {
+    //     const timer = setTimeout(() => {
+    //         setLetterClass('text-animate-hover');
+    //     }, 3000);
 
-        return () => {
-            clearTimeout(timer);
-        }
-    });
+    //     return () => {
+    //         clearTimeout(timer);
+    //     }
+    // });
 
-    useEffect(() => {
-        getPortfolio();
-    }, []);
+    // useEffect(() => {
+    //     getPortfolio();
+    // }, []);
 
-    const getPortfolio = async () => {
-        const querySnapshot = await getDocs(collection(db, 'portfolio'));
-        setPortfolio(querySnapshot.docs.map((doc) => doc.data()));
-    }
+    // const getPortfolio = async () => {
+    //     const querySnapshot = await getDocs(collection(db, 'portfolio'));
+    //     setPortfolio(querySnapshot.docs.map((doc) => doc.data()));
+    // }
 
     const renderPortfolio = (portfolio) => {
         return (
@@ -56,21 +56,21 @@ const Portfolio = () => {
     }
 
 
-    return (
-        <>
-            <div className="container portfolio-page">
-                <h1 className="page-title">
-                    <AnimatedLetters
-                        letterClass={letterClass}
-                        strArray={"Portfolio".split("")}
-                        idx={15}
-                    />
-                </h1>
-                <div>{renderPortfolio(portfolio)}</div>
-            </div>
-            <Loader type="pacman" />
-        </>
-    );
+    // return (
+    //     <>
+    //         <div className="container portfolio-page">
+    //             <h1 className="page-title">
+    //                 <AnimatedLetters
+    //                     letterClass={letterClass}
+    //                     strArray={"Portfolio".split("")}
+    //                     idx={15}
+    //                 />
+    //             </h1>
+    //             <div>{renderPortfolio(portfolio)}</div>
+    //         </div>
+    //         <Loader type="pacman" />
+    //     </>
+    // );
 }
 
 export default Portfolio;
